d30bfd844651c2e74fff537279a8453d31883ac6
Adriano <adriano.jesus2@fatec.sp.gov.br>
2015-08-22 23:45:01 -0300

    Implementação do Login do facebook

---
+++ app/src/main/java/br/com/compremelhor/DashboardActivity.java
@@ -0,0 +1,16 @@
+package br.com.compremelhor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by adriano on 21/08/15.
+ */
+public class DashboardActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.dashboard);
+    }
+}
---
+++ app/src/main/java/br/com/compremelhor/useful/Constants.java
@@ -0,0 +1,9 @@
+package br.com.compremelhor.useful;
+
+/**
+ * Created by adriano on 22/08/15.
+ */
+public interface Constants {
+    String PREFERENCES = "global_preferences";
+    String KEEP_CONNECT = "keep_connected";
+}
