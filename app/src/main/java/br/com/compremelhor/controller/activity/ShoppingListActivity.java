13538f9dd10d01a0b038590284390b10c1813b4a
Adriano <adriano.jesus2@fatec.sp.gov.br>
2015-10-18 12:32:19 -0200

    First Step: Screen Prototype

--- app/src/main/java/br/com/compremelhor/controller/activity/ShoppingListActivity.java
+++ app/src/main/java/br/com/compremelhor/controller/activity/ShoppingListActivity.java
@@ -1,11 +1,15 @@
 package br.com.compremelhor.controller.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,12 +22,19 @@ import br.com.compremelhor.controller.adapter.ExpandableListAdapter;
  * Created by adriano on 16/10/15.
  */
 public class ShoppingListActivity extends Activity
-        implements ExpandableListView.OnGroupExpandListener, ExpandableListView.OnGroupCollapseListener {
+        implements AdapterView.OnItemClickListener, DialogInterface.OnClickListener {
     private ExpandableListAdapter listAdapter;
     private ExpandableListView explictView;
     private List<String> listDataHeader;
     private HashMap<String, List<String>> listDataChild;
-    private ScrollView scrollView;
+
+    private int productSelected;
+
+    private Button btnAddProduct;
+    private Button btnFinishPurchase;
+
+    private AlertDialog alertDialog;
+    private AlertDialog alertDialogConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +43,50 @@ public class ShoppingListActivity extends Activity
 
         explictView = (ExpandableListView) findViewById(R.id.lv_shopping_list);
 
-        scrollView = (ScrollView) findViewById(R.id.svShoppingList);
-
-
         prepareListData();
 
         listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
         explictView.setAdapter(listAdapter);
-        }
 
-    @Override
-    public void onGroupExpand(int groupPosition) {
-        LinearLayout.LayoutParams param = (LinearLayout.LayoutParams) explictView.getLayoutParams();
-        param.height = ( explictView.getChildCount() * explictView.getHeight());
-        Log.v("SCRIP", "height -->>" + param.height);
-        explictView.setLayoutParams(param);
-        explictView.refreshDrawableState();
-        scrollView.refreshDrawableState();
+        setWidgets();
     }
 
-    @Override
-    public void onGroupCollapse(int groupPosition) {
-        LinearLayout.LayoutParams param = (LinearLayout.LayoutParams) explictView.getLayoutParams();
-        param.height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        Log.v("SCRIP", "height -->>" + param.height);
-        explictView.setLayoutParams(param);
-        explictView.refreshDrawableState();
-        scrollView.refreshDrawableState();
+    private void showMessage(String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    private void setWidgets() {
+        btnAddProduct = (Button) findViewById(R.id.btn_shopping_list_add);
+        btnFinishPurchase = (Button) findViewById(R.id.btn_shoppint_list_finish);
+
+        explictView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+
+                showMessage(listDataHeader.get(groupPosition)
+                        + ":"
+                        + listDataChild.get(listDataHeader.get(groupPosition)).get(childPosition));
+
+                productSelected = childPosition;
+                alertDialog.show();
+                return false;
+            }
+        });
+
+        alertDialog = createAlertDialog();
+        alertDialogConfirmation = createDialogConfirmation();
+    }
+
+
+    private AlertDialog createDialogConfirmation() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.confirm_delete);
+        builder.setPositiveButton(getString(R.string.yes), this);
+        builder.setNegativeButton(getString(R.string.no), this);
+        return builder.create();
     }
 
 
@@ -67,25 +94,25 @@ public class ShoppingListActivity extends Activity
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
 
-        listDataHeader.add("CATEGORIA CARNES");
-        listDataHeader.add("CATEGORIA BEBIDAS");
-        listDataHeader.add("CATEGORIA GULOSEIMAS");
+        listDataHeader.add("CATEGORIA CARNES    R$ 30,00");
+        listDataHeader.add("CATEGORIA BEBIDAS   R$ 30,00");
+        listDataHeader.add("CATEGORIA GULOSEIMAS    R$ 30,00");
 
         List<String> carnes = new ArrayList<String>();
-        carnes.add("Passarinho");
-        carnes.add("Frango");
-        carnes.add("hamburguer");
+        carnes.add("Passarinho          R$ 10,00");
+        carnes.add("Frango              R$ 10,00");
+        carnes.add("hamburguer          R$ 10,00");
 
 
         List<String> drinks = new ArrayList<String>();
-        drinks.add("Coca-cola");
-        drinks.add("Dell-Vale");
-        drinks.add("Guarana Antertica");
+        drinks.add("Coca-cola           R$ 10,00");
+        drinks.add("Dell-Vale           R$ 10,00");
+        drinks.add("Guarana Antertica   R$ 10,00");
 
         List<String> sweets = new ArrayList<String>();
-        sweets.add("passa-tempo");
-        sweets.add("chocolate nestle");
-        sweets.add("sorvete");
+        sweets.add("passa-tempo         R$ 10,00");
+        sweets.add("chocolate nestle    R$ 10,00");
+        sweets.add("sorvete             R$ 10,00");
 
         listDataChild.put(listDataHeader.get(0), carnes);
         listDataChild.put(listDataHeader.get(1), drinks);
@@ -93,4 +120,52 @@ public class ShoppingListActivity extends Activity
     }
 
 
+    private AlertDialog createAlertDialog() {
+        final CharSequence[] items;
+
+        items = new CharSequence[] {
+                getString(R.string.alter_quantity),
+                getString(R.string.remove_product),
+                getString(R.string.add_product)
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.options));
+        builder.setItems(items, this);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int item) {
+        Intent intent;
+
+        switch (item) {
+            case 0:
+                showMessage("Alterar quantidade");
+                break;
+
+            case 1:
+                alertDialogConfirmation.show();
+                break;
+
+            case 2:
+                showMessage("Adicionar Produto");
+                break;
+
+            case DialogInterface.BUTTON_POSITIVE:
+                break;
+
+            case DialogInterface.BUTTON_NEGATIVE:
+                alertDialogConfirmation.dismiss();
+                break;
+        }
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        this.productSelected = position;
+        alertDialog.show();
+    }
 }
