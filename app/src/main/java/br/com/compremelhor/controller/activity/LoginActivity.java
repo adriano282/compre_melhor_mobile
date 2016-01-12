03a67b570155a1941ab1c5d11cd16886a644c2d5
Adriano <adriano.jesus2@fatec.sp.gov.br>
2016-01-11 22:53:56 -0200

    Refactoring LoginActivity

--- app/src/main/java/br/com/compremelhor/controller/activity/LoginActivity.java
+++ app/src/main/java/br/com/compremelhor/controller/activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package br.com.compremelhor.controller.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,8 +9,8 @@ import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -19,7 +20,6 @@ import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.common.SignInButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,20 +29,26 @@ import java.util.Arrays;
 import br.com.compremelhor.R;
 import br.com.compremelhor.dao.DAOUser;
 import br.com.compremelhor.model.User;
-import br.com.compremelhor.useful.Constants;
 
-/**
- * Created by adriano on 18/08/15.
- */
-public class LoginActivity extends Activity implements Constants {
-    private LoginButton lgbFacebook;
+import static br.com.compremelhor.useful.Constants.KEEP_CONNECT;
+import static br.com.compremelhor.useful.Constants.PREFERENCES;
+import static br.com.compremelhor.useful.Constants.USER_ID;
+
+public class LoginActivity extends Activity {
     private final String TAG = "LoginActivity";
+
+    private LoginButton lgbFacebook;
     private CallbackManager callbackManager;
+
     private SharedPreferences preferences;
-    private EditText user, password;
-    private CheckBox keepConnected;
-    private SignInButton signGooglePlus;
-    private Long userId;
+    private EditText edUser, edPassword;
+    private CheckBox cbKeepConnected;
+    private DAOUser dao;
+
+    {
+        preferences = getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+        dao = new DAOUser(LoginActivity.this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,68 +56,99 @@ public class LoginActivity extends Activity implements Constants {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.login);
 
-        signGooglePlus = (SignInButton) findViewById(R.id.login_teste);
-        user = (EditText) findViewById(R.id.user);
-        password = (EditText) findViewById(R.id.password);
-        keepConnected = (CheckBox) findViewById(R.id.keep_connected);
+        edUser = (EditText) findViewById(R.id.user);
+        edPassword = (EditText) findViewById(R.id.password);
+        cbKeepConnected = (CheckBox) findViewById(R.id.keep_connected);
 
-        preferences = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
-
-
-        keepConnected.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    SharedPreferences.Editor edit = preferences.edit();
-                    edit.putBoolean(Constants.KEEP_CONNECT, true);
-                    edit.commit();
-                } else {
-                    SharedPreferences.Editor edit = preferences.edit();
-                    edit.putBoolean(Constants.KEEP_CONNECT, false);
-                    edit.commit();
-                }
-            }
-        });
-
-
+        cbKeepConnected.setOnCheckedChangeListener(new CheckButtonListener());
         lgbFacebook = (LoginButton) findViewById(R.id.login_button_facebook);
 
         lgbFacebook.setReadPermissions(Arrays.asList("public_profile", "email"));
         callbackManager = CallbackManager.Factory.create();
 
-        lgbFacebook.registerCallback(callbackManager,mFacebookCallback);
+        lgbFacebook.registerCallback(callbackManager, new FaceCallback());
+    }
 
-        if (preferences.getBoolean(KEEP_CONNECT, false) && AccessToken.getCurrentAccessToken() != null) {
-            initDashboard();
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
 
     }
 
-    private FacebookCallback<LoginResult> mFacebookCallback =  new FacebookCallback<LoginResult>() {
-        DAOUser dao = new DAOUser(LoginActivity.this);
+    public void onLogin() {
+        if (identification()) {
+            initDashboard();
+        }
+        Toast.makeText(this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT);
+    }
 
+    public void onRegister() {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    private boolean identification() {
+        String username = edUser.getText().toString();
+        String password = edPassword.getText().toString();
+
+        if (username.isEmpty())
+            return false;
+
+        User user = dao.getUserByEmail(username);
+        putUserId(user);
+        return user != null && user.getPassword().equals(password);
+    }
+
+    private void putUserId(User user) {
+        if (user != null)
+            preferences.edit().putLong(USER_ID, user.getId()).commit();
+    }
+
+    private void initDashboard() {
+        startActivity(new Intent(this, DashboardActivity.class));
+    }
+
+    private class CheckButtonListener implements  CompoundButton.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                preferences.edit().putBoolean(KEEP_CONNECT, true).commit();
+                return;
+            }
+            preferences.edit().putBoolean(KEEP_CONNECT, false).commit();
+        }
+    }
+
+    private class FaceCallback implements FacebookCallback<LoginResult> {
         @Override
         public void onSuccess(LoginResult loginResult) {
-
             GraphRequest request = GraphRequest.newMeRequest(
-                    loginResult.getAccessToken(),
-                    new GraphRequest.GraphJSONObjectCallback() {
+                    loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                         @Override
-                        public void onCompleted(JSONObject object,
-                                                GraphResponse response) {
-                            Log.v(TAG, response.toString());
-                            try {
-                                userId = dao.insertOrUpdate(
-                                        new User(object.getString("name"), object.getString("email"))
-                                );
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+                            User user = new User();
 
-                                SharedPreferences.Editor editor = preferences.edit();
-                                editor.putLong("USER_ID", userId);
-                                editor.commit();
-                                Log.v(TAG, "User id: " + userId);
+                            try {
+                                user.setName(object.getString("name"));
+                                user.setEmail(object.getString("email"));
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
+
+                            user.setId(dao.insertOrUpdate(user));
+                            putUserId(user);
                         }
                     }
             );
@@ -125,43 +162,11 @@ public class LoginActivity extends Activity implements Constants {
         }
 
         @Override
-        public void onCancel() {
-            Log.d(TAG, "An error occur during login of user.");
-        }
+        public void onCancel() {}
 
         @Override
         public void onError(FacebookException error) {
-            Log.e(TAG, "An error occur during login of user.");
+            Log.d(TAG, "Occurred a error attempting to login on facebook");
         }
-    };
-
-
-    private void initDashboard() {
-        Intent intent = new Intent(this, DashboardActivity.class);
-        startActivity(intent);
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        AppEventsLogger.activateApp(this);
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        AppEventsLogger.deactivateApp(this);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-
     }
 }
---
+++ app/src/main/java/br/com/compremelhor/controller/activity/RegisterActivity.java
@@ -0,0 +1,9 @@
+package br.com.compremelhor.controller.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by adriano on 11/01/16.
+ */
+public class RegisterActivity extends Activity {
+}
