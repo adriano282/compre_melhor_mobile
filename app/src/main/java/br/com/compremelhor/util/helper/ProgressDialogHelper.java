c9ee04cde5bca73a05dd8799df599107d3c32d52
Adriano <adriano.jesus2@fatec.sp.gov.br>
2016-08-07 09:25:00 -0300

    Creation of ProgressDialogHelper class

---
+++ app/src/main/java/br/com/compremelhor/util/helper/ProgressDialogHelper.java
@@ -0,0 +1,36 @@
+package br.com.compremelhor.util.helper;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import br.com.compremelhor.R;
+
+/**
+ * Created by adriano on 07/08/16.
+ */
+public class ProgressDialogHelper {
+    private static ProgressDialogHelper instance;
+
+    private Context context;
+    private ProgressDialog progressDialog;
+    private String message;
+
+    private ProgressDialogHelper() {}
+
+    public ProgressDialogHelper getInstance(@NonNull Context context, String message) {
+        if (instance == null) {
+            instance = new ProgressDialogHelper(); }
+
+        this.context = context;
+        this.message = message;
+        return instance;
+    }
+
+    public void showWaitProgressDialog() {
+        progressDialog = ProgressDialog
+                .show(context,
+                        context.getString(R.string.dialog_header_wait),
+                        message, true, false);
+    }
+}
